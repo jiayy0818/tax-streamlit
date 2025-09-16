b37da43ba751e48809f3176d0f0c8512698225ab
Jiayu Huang <22307100130@m.fudan.edu.cn>
2025-09-16 11:01:09 +0800

    Refactor tax calculator with updated inputs and logic

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import pandas as pd
 
+url = 'https://raw.githubusercontent.com/jiayy0818/tax-streamlit/main/%E4%BA%94%E9%99%A9%E4%B8%80%E9%87%91rule.xlsx'
+df = pd.read_excel(url, engine="openpyxl")
+ls_entity = list(set(df['RGB']))
+ls_city = list(set(df['缴纳地']))
+
+from ws_calculation import ws_cal
+from sd_calculation import sd_cal
+from sad_calculation import sad_cal
+from od_calculation import od_cal
+
+
+from ati_calculation import  ati_cal
 from tax_calculation import tax_cal
-from ati_calculation import ws_cal, sad_cal, od_cal, ati_cal
+
 
 st.title("Tax Deduction Calculator")
 st.caption("此计算器仅供员工选择年金个人缴费比例测试使用")
@@ -10,7 +23,8 @@ st.caption("此计算器仅供员工选择年金个人缴费比例测试使用")
 st.subheader("个人信息")
 grade = st.selectbox("职级：",options=["1","2","3","4","5","6","7","8","9","SL1","SL2","SL3"])
 # genre:蓝领/白领
-city = st.selectbox("所在城市:", ["上海","苏州","南京","长沙","成都","重庆","杭州"])
+entity = st.selectbox("所属公司:",options=ls_entity)
+city = st.selectbox("社保缴纳地:",options=ls_city)
 yos = st.selectbox("服务年限：",options=range(1,51))
 plantype = st.selectbox("原计划组成情况",options=["仅养老计划","储蓄计划和养老计划组合"])
 
@@ -23,21 +37,10 @@ col1, col2, col3, col4, col5 = st.columns([1, 0.05, 1, 0.05, 1])
 with col1:
     st.subheader("综合收入所得")
 
-    ms = st.number_input("工资月薪（税前）：",value=3000, min_value=3000)
+    ms = st.number_input("工资月薪（税前）：",value=8000, min_value=3000)
     month = st.selectbox("工资月数：",options=[12,13])
-
     allowance = st.number_input("每月津贴：", value=1000, min_value=0)
-
-    #bonus 按照级别的目标年终奖比例，计算好放入
-    bonus_map = {
-        "6": 0.1875, "7": 0.1875, "8": 0.1875, "9": 0.1875,
-        "SL1": 0.225,
-        "SL2": 0.2625,
-        "SL3": 0.4125
-    }
-    bonus_percent = bonus_map.get(grade, 0)
-    bonus = bonus_percent * ms * month
-    bonus_choice = st.selectbox("奖金是否单独计税：",options=["奖金单独计税","奖金不单独计税"])
+    bonus_choice = st.selectbox("奖金是否单独计税：", options=["奖金单独计税", "奖金不单独计税"])
 
     sr = st.number_input("劳务报酬所得:", value=0, min_value=0)
     mr = st.number_input("稿酬所得:", value=0, min_value=0)
@@ -85,25 +88,24 @@ st.divider()
 
 if st.button("计算个人所得税优惠"):
     #工资、薪酬所得
-    ws = ws_cal(ms, month, allowance, bonus, bonus_choice, yos, plantype)
+
+    ws = ws_cal(ms, month, allowance, grade, bonus_choice, yos, plantype)
+    st.write('工资、薪酬所得',ws,'元/年。')
 
     #缴纳基数
     salary = ws/12
-    cityavg_map = {} #社平工资字典
-    cityavg = cityavg_map.get(city, 10000)  #社平工资
-    base = min(3*cityavg, salary)
 
     # 专项附加扣除
     sad = sad_cal(baby, ce, ace, pqce, mimonth, hrmonth, city, aged, sibling, medical)
+    st.write('专项附加扣除',sad,'元/年。')
 
     #专项扣除
-    sd_map = {}
-    sd_percent = sd_map.get(city, 0)
-    sd = sd_percent * base * 12 #个人缴纳五险一金
+    sd = sd_cal(df, entity, city, salary)
+    st.write('专项扣除',sd,'元/月。')
 
     #其他扣除
-    od_org = od_cal(6, salary, cityavg,  tahi, tari) #不缴年金的其他扣除
-    od_new = od_cal(ec, salary, cityavg, tahi, tari) #缴年金的其他扣除
+    od_org = od_cal(df,6, salary, entity, city, tahi, tari)[0] #不缴年金的其他扣除
+    od_new, ec_amount = od_cal(df, ec, salary, entity, city, tahi, tari) #缴年金的其他扣除
 
     ati_org = ati_cal(ws, sr, mr, ri, sd, sad, od_org, donation)
     ati_new = ati_cal(ws, sr, mr, ri, sd, sad, od_new, donation)
@@ -112,6 +114,9 @@ if st.button("计算个人所得税优惠"):
     tax_new = tax_cal(ati_new)
     tax_deduction = tax_org - tax_new
 
+
     st.write("不缴年金时，全年应纳税所得额为", ati_org,"元；应缴纳个人所得税",tax_org,"元。")
     st.write("选择",ec,"档个人年金缴费比例时，全年应纳税所得额为",ati_new, "元；应缴纳个人所得税", tax_new, "元。")
-    st.write("应缴纳个人所得税优惠金额为", tax_deduction,"元。")
+
+    st.write('缴纳年金金额为:',ec_amount,'元/年。')
+    st.write("应缴纳个人所得税优惠金额为", tax_deduction,"元/年。")
