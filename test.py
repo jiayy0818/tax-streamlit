7c433249b25fc75c2f58d1668b24a0961b769854
Jiayu Huang <22307100130@m.fudan.edu.cn>
2025-09-16 11:40:02 +0800

    Remove engine specification in read_excel call

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 url = 'https://raw.githubusercontent.com/jiayy0818/tax-streamlit/main/%E4%BA%94%E9%99%A9%E4%B8%80%E9%87%91rule.xlsx'
-df = pd.read_excel(url, engine="openpyxl")
+df = pd.read_excel(url)
 ls_entity = list(set(df['RGB']))
 ls_city = list(set(df['缴纳地']))
 
